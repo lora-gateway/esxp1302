75990bcc542ec100eded606fc74596966125f4f7
Dennis Deng <ddeng@semtech.com>
2020-04-29 18:52:55 +0800

    SPI communication works

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -1,15 +1,4 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
-/* SPI Master example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
+/* SPI Testing for ESXP1302 Pkt-Fwd
 
    Unless required by applicable law or agreed to in writing, this
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
@@ -17,37 +6,86 @@
 */
 
 #include <stdio.h>
-#include "sdkconfig.h"
+#include <stdlib.h>
+#include <string.h>
+//#include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
-#include "esp_spi_flash.h"
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
+
+
+#define PIN_NUM_MISO 12
+#define PIN_NUM_MOSI 13
+#define PIN_NUM_CLK  14
+#define PIN_NUM_CS   15
+
+#define SX1302_SPI_HOST    HSPI_HOST
+#define DMA_CHAN    2
+
+// get sx1302 version
+uint8_t spi_get_version(spi_device_handle_t spi, const uint8_t *buf, int len)
+{
+    esp_err_t ret;
+    spi_transaction_t t;
+    uint8_t rbuf[16];
+
+    memset(&t, 0, sizeof(t));
+    t.length = 8 * len;
+    t.tx_buffer = buf;
+    t.rx_buffer = rbuf;
+
+    ret = spi_device_polling_transmit(spi, &t);
+    assert(ret == ESP_OK);
+
+    // printf("version: 0x%x\n", rbuf[4]);
+    return rbuf[4];
+}
+
 
 void app_main(void)
 {
-    printf("Hello world!\n");
+    uint8_t version;
+    esp_err_t ret;
+    spi_device_handle_t spi;
+    uint8_t buf[] = {0x00, 0x56, 0x06, 0x00, 0x00};
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU cores, WiFi%s%s, ",
-            CONFIG_IDF_TARGET,
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+    spi_bus_config_t buscfg = {
+        .miso_io_num = PIN_NUM_MISO,
+        .mosi_io_num = PIN_NUM_MOSI,
+        .sclk_io_num = PIN_NUM_CLK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+    };
+    spi_device_interface_config_t devcfg = {
+        .clock_speed_hz = 10*1000*1000,   // clock = 10 MHz
+        .mode = 0,
+        .spics_io_num = PIN_NUM_CS,
+        .queue_size = 8,
+    };
 
-    printf("silicon revision %d, ", chip_info.revision);
+    printf("Testing ESXP1302 SPI...\n");
 
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Free heap: %d\n", esp_get_free_heap_size());
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
+    for(int i = 0; i < 6; i++){
+        printf("waiting %d...\n", i+1);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+
+    // Initialize the SPI bus
+    ret = spi_bus_initialize(SX1302_SPI_HOST, &buscfg, DMA_CHAN);
+    ESP_ERROR_CHECK(ret);
+
+    // Attach SX1302 to the SPI bus
+    ret = spi_bus_add_device(SX1302_SPI_HOST, &devcfg, &spi);
+    ESP_ERROR_CHECK(ret);
+
+    // Get SX1302 Versioin
+    version = spi_get_version(spi, buf, 5);
+    printf("version = 0x%x\n", version);
+
+    while(true){
+        printf("hello\n");
+        vTaskDelay(8000 / portTICK_PERIOD_MS);
+    }
 }
