645b72702c349d270aaa7db2e0f802e7cbafba3e
Dennis Deng <ddeng@semtech.com>
2020-05-21 12:00:07 +0800

    add VERSION and config.h

---
+++ main/config.h
@@ -0,0 +1,16 @@
+#ifndef _LORAGW_CONFIGURATION_H
+#define _LORAGW_CONFIGURATION_H
+	#define LIBLORAGW_VERSION	"1.0.5"
+	#define DEBUG_AUX	0
+	#define DEBUG_SPI	0
+	#define DEBUG_I2C	0
+	#define DEBUG_REG	0
+	#define DEBUG_HAL	0
+	#define DEBUG_GPS	0
+	#define DEBUG_GPIO	
+	#define DEBUG_LBT	0
+	#define DEBUG_RAD	0
+	#define DEBUG_CAL	0
+	#define DEBUG_SX1302	0
+#endif
+
