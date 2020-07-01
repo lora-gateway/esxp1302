81f60bdac6913ea38198a046e42581965e7364a3
Dennis Deng <ddeng@semtech.com>
2020-07-01 15:32:37 +0800

    produce C head file as 'main/global_json.h'

--- test.sh
+++ test.sh
@@ -9,6 +9,13 @@ bootloader="0x1000 /home/dennis/mit/sx1302-esp32-pkt-fwd/build/bootloader/bootlo
 app="0x10000 /home/dennis/mit/sx1302-esp32-pkt-fwd/build/ESXP1302-Pkt-Fwd.bin"
 part="0x8000 /home/dennis/mit/sx1302-esp32-pkt-fwd/build/partitions_singleapp.bin"
 
+hd_name=main/global_json.h
+json_file=main/conf/global_conf.cn490.json
+
+# prepare the C array comes from global_conf.json
+echo 'static uint8_t global_conf[] = {' > $hd_name
+scripts/json_to_hex_array.py $json_file >> $hd_name
+echo '};' >> $hd_name
 
 make
 if [ $? -eq 0 ]; then
