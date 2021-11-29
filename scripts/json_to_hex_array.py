a8e5a9d17fc613c7a87c335da0d3535db45de08c
Dennis Deng <ddeng@semtech.com>
2021-11-29 17:40:17 +0800

    porting scripts/udp_client.py to support both python2 and python3

--- scripts/json_to_hex_array.py
+++ scripts/json_to_hex_array.py
@@ -10,14 +10,14 @@ with open(sys.argv[1]) as f:
 # print the length as the first 2 bytes
 n = len(chars)
 if n > 0xffff:
-    print "File size excel 65535. Abort"
+    print("File size excel 65535. Abort")
     sys.exit()
 
-print "0x%02X," %(n / 256),
-print "0x%02X," %(n % 256),
+print("0x%02X," %(n // 256)),
+print("0x%02X," %(n % 256)),
 
 for (i, x) in enumerate(chars, 3):
-    print "0x%02X," %ord(x),
+    print("0x%02X," %ord(x)),
     if i % 16 == 0:
         print
-print '0x00'  # add a '\0' to the end as string terminator
+print('0x00')  # add a '\0' to the end as string terminator
