f728d6b2d87b03ddbeb736b427f147e0579e1657
Dennis.Deng <ddeng@semtech.com>
2022-07-15 19:13:21 +0800

    update upgrader.py to correctly deal with port option

--- scripts/upgrader.py
+++ scripts/upgrader.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 # Upgrader for upgrading released ESXP32 images
+# Run 'pyinstaller --onefile upgrader.py' to produce frozen binary
 #
 # Dennis Deng (ddeng@semtech.com)
 # 2022.07.15
@@ -8,14 +9,47 @@
 
 import os
 import sys
+import serial
+import serial.tools.list_ports
 import subprocess
 
-cmd_str = 'esptool.py --port /dev/ttyUSB0 --baud 921600 --chip esp32 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect'
 
-if len(sys.argv) != 2 and len(sys.argv) != 4:
-    print("Usage:\n\tupgrader ESXP1302-Pkt-Fwd.bin")
-    print("Or:\tupgrader ESXP1302-Pkt-Fwd.bin bootloader.bin partition-table.bin")
+def get_port_name():
+    brand_list = ['USB']
+    ports = serial.tools.list_ports.comports()
+    ports = [str(p) for p in ports]
+    for p in ports:
+        for brand_str in brand_list:
+            if p.find(brand_str) >= 0:
+                return p.split()[0]
+
+    # no port found
+    print("Error! No port found which belongs to list %s!" %brand_list)
+    print("Only below ports available:")
+    for p in ports: print(p)
     sys.exit()
+
+
+cmd_str = 'esptool.py --baud 921600 --chip esp32 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect'
+
+if len(sys.argv) != 2 and len(sys.argv) != 4 and len(sys.argv) != 6:
+    print("Usage:\n\tupgrader ESXP1302-Pkt-Fwd.bin")
+    print("Or:\tupgrader ESXP1302-Pkt-Fwd.bin -p COM5")
+    print("Or:\tupgrader ESXP1302-Pkt-Fwd.bin bootloader.bin partition-table.bin")
+    print("Or:\tupgrader ESXP1302-Pkt-Fwd.bin bootloader.bin partition-table.bin -p COM5")
+    sys.exit()
+
+com = ''
+for i, x in enumerate(sys.argv):
+    if x == '-p':
+        com = sys.argv[i+1]
+        sys.argv.remove(sys.argv[i+1])
+        sys.argv.remove(sys.argv[i])
+        break
+
+if com == '':
+    com = get_port_name()
+
 
 # check files exist or not
 if not os.path.exists(sys.argv[1]):
@@ -30,10 +64,11 @@ if len(sys.argv) == 4:
         sys.exit()
 
 
-print("Note: you may need to press either esp32 button before or during the upgrade process")
-input("Enter any key to start...")
+print("\nNote: you may need to press either esp32 button before or during the upgrade process\n")
+#input("Press 'Enter' to start...")
 
 cmd_list = cmd_str.split()
+cmd_list[1:1] = ['--port', com]  # put it in the same position
 cmd_list += ['0x10000', sys.argv[1]]
 
 if len(sys.argv) == 4:
@@ -44,4 +79,6 @@ if len(sys.argv) == 4:
         cmd_list += ['0x1000', sys.argv[2]]
         cmd_list += ['0x8000', sys.argv[3]]
 
+print('cmd:', ' '.join(cmd_list))
 subprocess.call(cmd_list)
+#subprocess.call(cmd_list, shell=True)  # this is for windows
