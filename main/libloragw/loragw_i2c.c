34e65ceec925d9725a594bb7202462e725c3c34f
dennis <ddeng@semtech.com>
2024-01-09 19:04:44 +0800

    port libloragw/loragw_i2c.*

--- main/libloragw/loragw_i2c.c
+++ main/libloragw/loragw_i2c.c
@@ -13,26 +13,16 @@ License: Revised BSD License, see LICENSE.TXT file include in the project
 */
 
 
-/* -------------------------------------------------------------------------- */
-/* --- DEPENDANCIES --------------------------------------------------------- */
-
 #include <stdint.h>     /* C99 types */
 #include <stdio.h>      /* printf fprintf */
 #include <unistd.h>     /* lseek, close */
 #include <fcntl.h>      /* open */
 #include <errno.h>      /* errno */
 
-#include <sys/ioctl.h>
-#include <linux/i2c.h>
-#include <linux/i2c-dev.h>
-
 #include "loragw_i2c.h"
 #include "loragw_aux.h"
 
-/* -------------------------------------------------------------------------- */
-/* --- PRIVATE MACROS ------------------------------------------------------- */
 
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 #if DEBUG_I2C == 1
     #define DEBUG_MSG(str)                fprintf(stdout, str)
     #define DEBUG_PRINTF(fmt, args...)    fprintf(stdout,"%s:%d: "fmt, __FUNCTION__, __LINE__, args)
@@ -43,138 +33,92 @@ License: Revised BSD License, see LICENSE.TXT file include in the project
     #define CHECK_NULL(a)                if(a==NULL){return LGW_I2C_ERROR;}
 #endif
 
-/* -------------------------------------------------------------------------- */
-/* --- PRIVATE CONSTANTS ---------------------------------------------------- */
 
-/* -------------------------------------------------------------------------- */
-/* --- PUBLIC FUNCTIONS DEFINITION ------------------------------------------ */
+static i2c_port_t i2c_num = I2C_MASTER_NUM;
+static bool i2c_opened = false;
 
-int i2c_linuxdev_open(const char *path, uint8_t device_addr, int *i2c_fd) {
-    int dev;
+esp_err_t i2c_esp32_open(void)
+{
+    i2c_config_t conf;
+    int i2c_master_port = i2c_num;
+    esp_err_t ret;
 
-    /* Check input variables */
-    if (path == NULL) {
-        DEBUG_MSG("ERROR: null pointer path\n");
-        return LGW_I2C_ERROR;
-    }
-    if (i2c_fd == NULL) {
-        DEBUG_MSG("ERROR: null pointer i2c_fd\n");
-        return LGW_I2C_ERROR;
-    }
+    if(i2c_opened == true)
+        return ESP_OK;
 
-    /* Open I2C device */
-    dev = open(path, O_RDWR);
-    if (dev < 0) {
-        DEBUG_PRINTF("ERROR: Failed to open I2C %s - %s\n", path, strerror(errno));
-        return LGW_I2C_ERROR;
-    }
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_MASTER_SDA_IO;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_io_num = I2C_MASTER_SCL_IO;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
+    conf.clk_flags = I2C_SCLK_SRC_FLAG_FOR_NOMAL;
+    i2c_param_config(i2c_master_port, &conf);
 
-    /* Setting I2C device mode to slave */
-    if (ioctl(dev, I2C_SLAVE, device_addr) < 0) {
-        DEBUG_PRINTF("ERROR: Failed to acquire bus access and/or talk to slave - %s\n", strerror(errno));
-        return LGW_I2C_ERROR;
-    }
-
-    DEBUG_PRINTF("INFO: I2C port opened successfully (%s, 0x%02X)\n", path, device_addr);
-    *i2c_fd = dev; /* return file descriptor index */
-
-    return LGW_I2C_SUCCESS;
-}
-
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-int i2c_linuxdev_read(int i2c_fd, uint8_t device_addr, uint8_t reg_addr, uint8_t *data) {
-    uint8_t *inbuff, outbuff;
-    struct i2c_rdwr_ioctl_data packets;
-    struct i2c_msg messages[2];
-
-    outbuff = reg_addr;
-    messages[0].addr = device_addr;
-    messages[0].flags= 0;
-    messages[0].len = sizeof(outbuff);
-    messages[0].buf = &outbuff;
-
-    inbuff = data;
-    messages[1].addr = device_addr;
-    messages[1].flags = I2C_M_RD;
-    messages[1].len = sizeof(*inbuff);
-    messages[1].buf = inbuff;
-
-    packets.msgs = messages;
-    packets.nmsgs = 2;
-
-    if (ioctl(i2c_fd, I2C_RDWR, &packets) < 0) {
-        DEBUG_PRINTF("ERROR: Read from I2C Device failed (%d, 0x%02x, 0x%02x) - %s\n", i2c_fd, device_addr, reg_addr, strerror(errno));
-        return LGW_I2C_ERROR;
-    }
-
-    return LGW_I2C_SUCCESS;
-}
-
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-int i2c_linuxdev_write(int i2c_fd, uint8_t device_addr, uint8_t reg_addr, uint8_t data) {
-    unsigned char buff[2];
-    struct i2c_rdwr_ioctl_data packets;
-    struct i2c_msg messages[1];
-
-    buff[0] = reg_addr;
-    buff[1] = data;
-
-    messages[0].addr = device_addr;
-    messages[0].flags = 0;
-    messages[0].len = sizeof(buff);
-    messages[0].buf = buff;
-
-    packets.msgs = messages;
-    packets.nmsgs = 1;
-
-    if (ioctl(i2c_fd, I2C_RDWR, &packets) < 0) {
-        DEBUG_PRINTF("ERROR: Write to I2C Device failed (%d, 0x%02x, 0x%02x) - %s\n", i2c_fd, device_addr, reg_addr, strerror(errno));
-        return LGW_I2C_ERROR;
-    }
-
-    return LGW_I2C_SUCCESS;
-}
-
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-int i2c_linuxdev_write_buffer(int i2c_fd, uint8_t device_addr, uint8_t *buffer, uint8_t size) {
-    struct i2c_rdwr_ioctl_data packets;
-    struct i2c_msg messages[1];
-
-    /* Check input parameters */
-    CHECK_NULL(buffer);
-
-    messages[0].addr = device_addr;
-    messages[0].flags = 0;
-    messages[0].len = size;
-    messages[0].buf = buffer;
-
-    packets.msgs = messages;
-    packets.nmsgs = 1;
-
-    if (ioctl(i2c_fd, I2C_RDWR, &packets) < 0) {
-        DEBUG_PRINTF("ERROR: Write buffer to I2C Device failed (%d, 0x%02x) - %s\n", i2c_fd, device_addr, strerror(errno));
-        return LGW_I2C_ERROR;
-    }
-
-    return LGW_I2C_SUCCESS;
-}
-
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-int i2c_linuxdev_close(int i2c_fd) {
-    int i;
-
-    i = close(i2c_fd);
-    if (i == 0) {
-        DEBUG_MSG("INFO: I2C port closed successfully\n");
-        return LGW_I2C_SUCCESS;
+    ret = i2c_driver_install(i2c_master_port, conf.mode,
+            I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+    if(ret == ESP_OK){
+        DEBUG_PRINTF("INFO: I2C port(%d) opened successfully\n", i2c_num);
+        i2c_opened = true;
     } else {
-        DEBUG_PRINTF("ERROR: Failed to close I2C - %s\n", strerror(errno));
-        return LGW_I2C_ERROR;
+        DEBUG_PRINTF("INFO: I2C port(%d) opened failed\n", i2c_num);
     }
+
+    return ret;
 }
 
-/* --- EOF ------------------------------------------------------------------ */
+esp_err_t i2c_esp32_read(uint8_t device_addr, uint8_t reg_addr, uint8_t *data)
+{
+    esp_err_t ret;
+
+    // write first, provide device address and register address
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (device_addr << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, reg_addr, ACK_CHECK_EN);
+
+    // now change to read, and save one byte to 'data'
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (device_addr << 1) | I2C_MASTER_READ, ACK_CHECK_EN);
+    i2c_master_read_byte(cmd, data, NACK_VAL);
+    i2c_master_stop(cmd);
+
+    ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+
+    return ret;
+}
+
+esp_err_t i2c_esp32_write(uint8_t device_addr, uint8_t reg_addr, uint8_t data)
+{
+    uint8_t buf[2];
+
+    buf[0] = reg_addr;
+    buf[1] = data;
+
+    return i2c_esp32_write_buf(device_addr, buf, 2);
+}
+
+esp_err_t i2c_esp32_write_buf(uint8_t device_addr, uint8_t *data, size_t size)
+{
+    esp_err_t ret;
+
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (device_addr << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
+    i2c_master_write(cmd, data, size, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+
+    ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+
+    return ret;
+}
+
+esp_err_t i2c_esp32_close(void)
+{
+    if(i2c_opened == true)
+        return i2c_driver_delete(i2c_num);
+    else
+        return ESP_OK;
+}
--- main/libloragw/loragw_i2c.h
+++ main/libloragw/loragw_i2c.h
@@ -12,74 +12,70 @@ Description:
 License: Revised BSD License, see LICENSE.TXT file include in the project
 */
 
-
 #ifndef _LORAGW_I2C_H
 #define _LORAGW_I2C_H
 
-/* -------------------------------------------------------------------------- */
-/* --- DEPENDANCIES --------------------------------------------------------- */
-
 #include <stdint.h>        /* C99 types*/
 
-#include "config.h"    /* library configuration options (dynamically generated) */
+#include "config.h"
+#include "driver/i2c.h"
 
-/* -------------------------------------------------------------------------- */
-/* --- PUBLIC CONSTANTS ----------------------------------------------------- */
 
-#define LGW_I2C_SUCCESS     0
+#define LGW_I2C_SUCCESS      0
 #define LGW_I2C_ERROR       -1
 
-#define I2C_DEVICE          "/dev/i2c-1"
+#define ACK_CHECK_EN       0x1      /* I2C master will check ack from slave */
+#define ACK_CHECK_DIS      0x0      /* I2C master will not check ack from slave */
+#define ACK_VAL            0x0      /* I2C ack value */
+#define NACK_VAL           0x1      /* I2C nack value */
 
-/* -------------------------------------------------------------------------- */
-/* --- PUBLIC FUNCTIONS PROTOTYPES ------------------------------------------ */
+#define I2C_MASTER_SCL_IO   22      /* gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO   21      /* gpio number for I2C master data  */
+#define I2C_MASTER_NUM       0      /* I2C port number for master dev */
+
+
+#define I2C_MASTER_TX_BUF_DISABLE  0        /* I2C master do not need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE  0        /* I2C master do not need buffer */
+#define I2C_MASTER_FREQ_HZ         100000   /* I2C master clock frequency */
+
 
 /**
 @brief Open I2C port
-@param path         Path to the I2C device driver (absolute or relative)
-@param device_addr  Address of the device
-@param i2c_fd      Pointer to receive I2C port file descriptor index
 @return 0 if I2C port was open successfully, -1 else
 */
-int i2c_linuxdev_open(const char *path, uint8_t device_addr, int *i2c_fd);
+esp_err_t i2c_esp32_open(void);
 
 /**
 @brief Close I2C port
-@param i2c_fd      I2C port file descriptor index
 @return 0 if I2C port was closed successfully, -1 else
 */
-int i2c_linuxdev_close(int i2c_fd);
+esp_err_t i2c_esp32_close(void);
 
 /**
 @brief Read data from an I2C port
-@param i2c_fd      I2C port file descriptor index
 @param device_addr  I2C device address
 @param reg_addr     Address of the register to be read
 @param data         Pointer to a buffer to store read data
 @return 0 if I2C data read is successful, -1 else
 */
-int i2c_linuxdev_read(int i2c_fd, uint8_t device_addr, uint8_t reg_addr, uint8_t *data);
+esp_err_t i2c_esp32_read(uint8_t device_addr, uint8_t reg_addr, uint8_t *data);
 
 /**
 @brief Write data to an I2C port
-@param i2c_fd      I2C port file descriptor index
 @param device_addr  I2C device address
 @param reg_addr     Address of the register to write to
 @param data         byte to write in the register
 @return 0 if I2C data write is successful, -1 else
 */
-int i2c_linuxdev_write(int i2c_fd, uint8_t device_addr, uint8_t reg_addr, uint8_t data);
+esp_err_t i2c_esp32_write(uint8_t device_addr, uint8_t reg_addr, uint8_t data);
 
 /**
 @brief Write a raw buffer to an I2C port
-@param i2c_fd       I2C port file descriptor index
 @param device_addr  I2C device address
-@param buffer       Buffer to be written to the device
+@param data         byte to write in the register
 @param size         Size of the buffer to be written
 @return 0 if I2C data write is successful, -1 else
 */
-int i2c_linuxdev_write_buffer(int i2c_fd, uint8_t device_addr, uint8_t *buffer, uint8_t size);
+esp_err_t i2c_esp32_write_buf(uint8_t device_addr, uint8_t *data, size_t size);
 
 #endif
-
-/* --- EOF ------------------------------------------------------------------ */
