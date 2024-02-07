1aabdf1e9edfaa83213d63c785e951fec21fbc29
dennis <ddeng@semtech.com>
2024-02-07 10:14:31 +0800

    add config.h directly instead of deriving from library.cfg

---
+++ main/libloragw/config.h
@@ -0,0 +1,19 @@
+#ifndef _LORAGW_CONFIGURATION_H
+#define _LORAGW_CONFIGURATION_H
+
+#define LIBLORAGW_VERSION    "2.1.0"
+#define DEBUG_AUX            0
+#define DEBUG_COM            0
+#define DEBUG_MCU            0
+#define DEBUG_I2C            0
+#define DEBUG_REG            0
+#define DEBUG_HAL            0
+#define DEBUG_GPS            0
+#define DEBUG_GPIO           0
+#define DEBUG_LBT            0
+#define DEBUG_RAD            0
+#define DEBUG_CAL            0
+#define DEBUG_SX1302         0
+#define DEBUG_FTIME          0
+
+#endif
