74a83d3419506c8716be3344b58f37ddb8cd1279
dennis <ddeng@semtech.com>
2024-02-23 18:21:53 +0800

    restore libloragw/loragw_reg.c to its original code

--- main/libloragw/loragw_reg.c
+++ main/libloragw/loragw_reg.c
@@ -23,7 +23,6 @@ License: Revised BSD License, see LICENSE.TXT file include in the project
 #include <stdbool.h>    /* bool type */
 #include <stdio.h>      /* printf fprintf */
 
-#include "loragw_spi.h"
 #include "loragw_reg.h"
 
 /* -------------------------------------------------------------------------- */
@@ -1112,61 +1111,38 @@ const struct lgw_reg_s loregs[LGW_TOTALREGS+1] = {
 /* --- PRIVATE VARIABLES ---------------------------------------------------- */
 
 /* -------------------------------------------------------------------------- */
-/* --- INTERNAL SHARED VARIABLES -------------------------------------------- */
-
-spi_device_handle_t *lgw_spi_target = NULL; /*! generic ptr to the SPI device */
-
-/* -------------------------------------------------------------------------- */
 /* --- PRIVATE FUNCTIONS ---------------------------------------------------- */
 
-int reg_w(void *spi_target, uint8_t spi_mux_target, struct lgw_reg_s r, int32_t reg_value) {
-    int spi_stat = LGW_REG_SUCCESS;
-    uint8_t buf[4] = "\x00\x00\x00\x00";
+int reg_w(uint8_t spi_mux_target, struct lgw_reg_s r, int32_t reg_value) {
+    int com_stat = LGW_REG_SUCCESS;
 
     if ((r.leng == 8) && (r.offs == 0)) {
         /* direct write */
+        com_stat = lgw_com_w(spi_mux_target, r.addr, (uint8_t)reg_value);
         DEBUG_PRINTF("==> DIRECT WRITE @ 0x%04X\n", r.addr);
-        spi_stat += lgw_spi_w(spi_target, spi_mux_target, r.addr, (uint8_t)reg_value);
     } else if ((r.offs + r.leng) <= 8) {
-        /* single-byte read-modify-write, offs:[0-7], leng:[1-7] */
+        /* read-modify-write */
+        com_stat = lgw_com_rmw(spi_mux_target, r.addr, r.offs, r.leng, (uint8_t)reg_value);
         DEBUG_PRINTF("==> READ MODIFY WRITE @ 0x%04X (offs:%u leng:%u)\n", r.addr, r.offs, r.leng);
-        spi_stat += lgw_spi_r(spi_target, spi_mux_target, r.addr, &buf[0]);
-        buf[1] = ((1 << r.leng) - 1) << r.offs; /* bit mask */
-        buf[2] = ((uint8_t)reg_value) << r.offs; /* new data offsetted */
-        buf[3] = (~buf[1] & buf[0]) | (buf[1] & buf[2]); /* mixing old & new data */
-        spi_stat += lgw_spi_w(spi_target, spi_mux_target, r.addr, buf[3]);
-#if 0  // TODO: below code is removed in v2.1.0; keep it here in case one day need checking
-    } else if ((r.offs == 0) && (r.leng > 0) && (r.leng <= 32)) {
-        int i, size_byte;
-        /* multi-byte direct write routine */
-        size_byte = (r.leng + 7) / 8; /* add a byte if it's not an exact multiple of 8 */
-        for (i=0; i<size_byte; ++i) {
-            /* big endian register file for a file on N bytes
-            Least significant byte is stored in buf[0], most one in buf[N-1] */
-            buf[i] = (uint8_t)(0x000000FF & reg_value);
-            reg_value = (reg_value >> 8);
-        }
-        spi_stat += lgw_spi_wb(spi_target, spi_mux_target, r.addr, buf, size_byte); /* write the register in one burst */
-#endif
     } else {
         /* register spanning multiple memory bytes but with an offset */
         DEBUG_MSG("ERROR: REGISTER SIZE AND OFFSET ARE NOT SUPPORTED\n");
         return LGW_REG_ERROR;
     }
 
-    return spi_stat;
+    return com_stat;
 }
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
-int reg_r(void *spi_target, uint8_t spi_mux_target, struct lgw_reg_s r, int32_t *reg_value) {
-    int spi_stat = LGW_REG_SUCCESS;
+int reg_r(uint8_t spi_mux_target, struct lgw_reg_s r, int32_t *reg_value) {
+    int com_stat = LGW_REG_SUCCESS;
     uint8_t bufu[4] = "\x00\x00\x00\x00";
     int8_t *bufs = (int8_t *)bufu;
 
     if ((r.offs + r.leng) <= 8) {
         /* read one byte, then shift and mask bits to get reg value with sign extension if needed */
-        spi_stat = lgw_spi_r(spi_target, spi_mux_target, r.addr, &bufu[0]);
+        com_stat = lgw_com_r(spi_mux_target, r.addr, &bufu[0]);
         bufu[1] = bufu[0] << (8 - r.leng - r.offs); /* left-align the data */
         if (r.sign == true) {
             bufs[2] = bufs[1] >> (8 - r.leng); /* right align the data with sign extension (ARITHMETIC right shift) */
@@ -1175,55 +1151,39 @@ int reg_r(void *spi_target, uint8_t spi_mux_target, struct lgw_reg_s r, int32_t 
             bufu[2] = bufu[1] >> (8 - r.leng); /* right align the data, no sign extension */
             *reg_value = (int32_t)bufu[2]; /* unsigned pointer -> no sign extension */
         }
-#if 0  // TODO: below code is removed in v2.1.0; keep it here in case one day need checking
-    } else if ((r.offs == 0) && (r.leng > 0) && (r.leng <= 32)) {
-        size_byte = (r.leng + 7) / 8; /* add a byte if it's not an exact multiple of 8 */
-        spi_stat += lgw_spi_rb(spi_target, spi_mux_target, r.addr, bufu, size_byte);
-        u = 0;
-        for (i=(size_byte-1); i>=0; --i) {
-            u = (uint32_t)bufu[i] + (u << 8); /* transform a 4-byte array into a 32 bit word */
-        }
-        if (r.sign == true) {
-            u = u << (32 - r.leng); /* left-align the data */
-            *reg_value = (int32_t)u >> (32 - r.leng); /* right-align the data with sign extension (ARITHMETIC right shift) */
-        } else {
-            *reg_value = (int32_t)u; /* unsigned value -> return 'as is' */
-        }
-#endif
     } else {
         /* register spanning multiple memory bytes but with an offset */
         DEBUG_MSG("ERROR: REGISTER SIZE AND OFFSET ARE NOT SUPPORTED\n");
         return LGW_REG_ERROR;
     }
 
-    return spi_stat;
+    return com_stat;
 }
 
 /* -------------------------------------------------------------------------- */
 /* --- PUBLIC FUNCTIONS DEFINITION ------------------------------------------ */
 
 /* Concentrator connect */
-int lgw_connect(const lgw_com_type_t com_type, const char * com_path)
-{
-    int spi_stat = LGW_SPI_SUCCESS;
+int lgw_connect(const lgw_com_type_t com_type, const char * com_path) {
+    int com_stat = LGW_COM_SUCCESS;
     uint8_t u = 0;
 
-    if (lgw_spi_target != NULL) {
-        DEBUG_MSG("WARNING: concentrator was already connected\n");
-        lgw_spi_close(lgw_spi_target);
+    /* check COM link status */
+    if (com_path == NULL) {
+        DEBUG_MSG("ERROR: COM PATH IS NOT SET\n");
+        return LGW_REG_ERROR;
     }
 
-    /* open the SPI link */
-    spi_stat = lgw_spi_open(&lgw_spi_target);
-    if (spi_stat != LGW_SPI_SUCCESS) {
+    /* open the COM link */
+    com_stat = lgw_com_open(com_type, com_path);
+    if (com_stat != LGW_COM_SUCCESS) {
         DEBUG_MSG("ERROR CONNECTING CONCENTRATOR\n");
         return LGW_REG_ERROR;
     }
 
     /* check SX1302 version */
-    //com_stat = lgw_com_r(LGW_SPI_MUX_TARGET_SX1302, loregs[SX1302_REG_COMMON_VERSION_VERSION].addr, &u);
-    spi_stat = lgw_spi_r(lgw_spi_target, LGW_SPI_MUX_TARGET_SX1302, loregs[SX1302_REG_COMMON_VERSION_VERSION].addr, &u);
-    if (spi_stat != LGW_SPI_SUCCESS) {
+    com_stat = lgw_com_r(LGW_SPI_MUX_TARGET_SX1302, loregs[SX1302_REG_COMMON_VERSION_VERSION].addr, &u);
+    if (com_stat != LGW_COM_SUCCESS) {
         DEBUG_MSG("ERROR READING CHIP VERSION REGISTER\n");
         return LGW_REG_ERROR;
     }
@@ -1237,13 +1197,14 @@ int lgw_connect(const lgw_com_type_t com_type, const char * com_path)
 
 /* Concentrator disconnect */
 int lgw_disconnect(void) {
-    if (lgw_spi_target != NULL) {
-        lgw_spi_close(lgw_spi_target);
-        lgw_spi_target = NULL;
+    int com_stat;
+
+    com_stat = lgw_com_close();
+    if (com_stat == LGW_COM_SUCCESS) {
         DEBUG_MSG("Note: success disconnecting the concentrator\n");
         return LGW_REG_SUCCESS;
     } else {
-        DEBUG_MSG("WARNING: concentrator was already disconnected\n");
+        DEBUG_MSG("ERROR: Failed to disconnect the concentrator\n");
         return LGW_REG_ERROR;
     }
 }
@@ -1252,18 +1213,12 @@ int lgw_disconnect(void) {
 
 /* Write to a register addressed by name */
 int lgw_reg_w(uint16_t register_id, int32_t reg_value) {
-    int spi_stat = LGW_SPI_SUCCESS;
+    int com_stat = LGW_COM_SUCCESS;
     struct lgw_reg_s r;
 
     /* check input parameters */
     if (register_id >= LGW_TOTALREGS) {
         DEBUG_MSG("ERROR: REGISTER NUMBER OUT OF DEFINED RANGE\n");
-        return LGW_REG_ERROR;
-    }
-
-    /* check if SPI is initialised */
-    if (lgw_spi_target == NULL) {
-        DEBUG_MSG("ERROR: CONCENTRATOR UNCONNECTED\n");
         return LGW_REG_ERROR;
     }
 
@@ -1276,10 +1231,10 @@ int lgw_reg_w(uint16_t register_id, int32_t reg_value) {
         return LGW_REG_ERROR;
     }
 
-    spi_stat += reg_w(lgw_spi_target, LGW_SPI_MUX_TARGET_SX1302, r, reg_value);
+    com_stat = reg_w(LGW_SPI_MUX_TARGET_SX1302, r, reg_value);
 
-    if (spi_stat != LGW_SPI_SUCCESS) {
-        DEBUG_MSG("ERROR: SPI ERROR DURING REGISTER WRITE\n");
+    if (com_stat != LGW_COM_SUCCESS) {
+        DEBUG_MSG("ERROR: COM ERROR DURING REGISTER WRITE\n");
         return LGW_REG_ERROR;
     } else {
         return LGW_REG_SUCCESS;
@@ -1290,7 +1245,7 @@ int lgw_reg_w(uint16_t register_id, int32_t reg_value) {
 
 /* Read to a register addressed by name */
 int lgw_reg_r(uint16_t register_id, int32_t *reg_value) {
-    int spi_stat = LGW_SPI_SUCCESS;
+    int com_stat = LGW_COM_SUCCESS;
     struct lgw_reg_s r;
 
     /* check input parameters */
@@ -1300,19 +1255,13 @@ int lgw_reg_r(uint16_t register_id, int32_t *reg_value) {
         return LGW_REG_ERROR;
     }
 
-    /* check if SPI is initialised */
-    if (lgw_spi_target == NULL) {
-        DEBUG_MSG("ERROR: CONCENTRATOR UNCONNECTED\n");
-        return LGW_REG_ERROR;
-    }
-
     /* get register struct from the struct array */
     r = loregs[register_id];
 
-    spi_stat += reg_r(lgw_spi_target, LGW_SPI_MUX_TARGET_SX1302, r, reg_value);
+    com_stat = reg_r(LGW_SPI_MUX_TARGET_SX1302, r, reg_value);
 
-    if (spi_stat != LGW_SPI_SUCCESS) {
-        DEBUG_MSG("ERROR: SPI ERROR DURING REGISTER WRITE\n");
+    if (com_stat != LGW_COM_SUCCESS) {
+        DEBUG_MSG("ERROR: COM ERROR DURING REGISTER WRITE\n");
         return LGW_REG_ERROR;
     } else {
         return LGW_REG_SUCCESS;
@@ -1323,7 +1272,7 @@ int lgw_reg_r(uint16_t register_id, int32_t *reg_value) {
 
 /* Point to a register by name and do a burst write */
 int lgw_reg_wb(uint16_t register_id, uint8_t *data, uint16_t size) {
-    int spi_stat = LGW_SPI_SUCCESS;
+    int com_stat = LGW_COM_SUCCESS;
     struct lgw_reg_s r;
 
     /* check input parameters */
@@ -1334,12 +1283,6 @@ int lgw_reg_wb(uint16_t register_id, uint8_t *data, uint16_t size) {
     }
     if (register_id >= LGW_TOTALREGS) {
         DEBUG_MSG("ERROR: REGISTER NUMBER OUT OF DEFINED RANGE\n");
-        return LGW_REG_ERROR;
-    }
-
-    /* check if SPI is initialised */
-    if (lgw_spi_target == NULL) {
-        DEBUG_MSG("ERROR: CONCENTRATOR UNCONNECTED\n");
         return LGW_REG_ERROR;
     }
 
@@ -1353,10 +1296,10 @@ int lgw_reg_wb(uint16_t register_id, uint8_t *data, uint16_t size) {
     }
 
     /* do the burst write */
-    spi_stat += lgw_spi_wb(lgw_spi_target, LGW_SPI_MUX_TARGET_SX1302, r.addr, data, size);
+    com_stat = lgw_com_wb(LGW_SPI_MUX_TARGET_SX1302, r.addr, data, size);
 
-    if (spi_stat != LGW_SPI_SUCCESS) {
-        DEBUG_MSG("ERROR: SPI ERROR DURING REGISTER BURST WRITE\n");
+    if (com_stat != LGW_COM_SUCCESS) {
+        DEBUG_MSG("ERROR: COM ERROR DURING REGISTER BURST WRITE\n");
         return LGW_REG_ERROR;
     } else {
         return LGW_REG_SUCCESS;
@@ -1367,7 +1310,7 @@ int lgw_reg_wb(uint16_t register_id, uint8_t *data, uint16_t size) {
 
 /* Point to a register by name and do a burst read */
 int lgw_reg_rb(uint16_t register_id, uint8_t *data, uint16_t size) {
-    int spi_stat = LGW_SPI_SUCCESS;
+    int com_stat = LGW_COM_SUCCESS;
     struct lgw_reg_s r;
 
     /* check input parameters */
@@ -1381,20 +1324,14 @@ int lgw_reg_rb(uint16_t register_id, uint8_t *data, uint16_t size) {
         return LGW_REG_ERROR;
     }
 
-    /* check if SPI is initialised */
-    if (lgw_spi_target == NULL) {
-        DEBUG_MSG("ERROR: CONCENTRATOR UNCONNECTED\n");
-        return LGW_REG_ERROR;
-    }
-
     /* get register struct from the struct array */
     r = loregs[register_id];
 
     /* do the burst read */
-    spi_stat += lgw_spi_rb(lgw_spi_target, LGW_SPI_MUX_TARGET_SX1302, r.addr, data, size);
+    com_stat = lgw_com_rb(LGW_SPI_MUX_TARGET_SX1302, r.addr, data, size);
 
-    if (spi_stat != LGW_SPI_SUCCESS) {
-        DEBUG_MSG("ERROR: SPI ERROR DURING REGISTER BURST READ\n");
+    if (com_stat != LGW_COM_SUCCESS) {
+        DEBUG_MSG("ERROR: COM ERROR DURING REGISTER BURST READ\n");
         return LGW_REG_ERROR;
     } else {
         return LGW_REG_SUCCESS;
@@ -1404,7 +1341,7 @@ int lgw_reg_rb(uint16_t register_id, uint8_t *data, uint16_t size) {
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 int lgw_mem_wb(uint16_t mem_addr, const uint8_t *data, uint16_t size) {
-    int spi_stat = LGW_SPI_SUCCESS;
+    int com_stat = LGW_COM_SUCCESS;
     int chunk_cnt = 0;
     uint16_t addr = mem_addr;
     uint16_t sz_todo = size;
@@ -1415,12 +1352,6 @@ int lgw_mem_wb(uint16_t mem_addr, const uint8_t *data, uint16_t size) {
     CHECK_NULL(data);
     if (size == 0) {
         DEBUG_MSG("ERROR: BURST OF NULL LENGTH\n");
-        return LGW_REG_ERROR;
-    }
-
-    /* check if SPI is initialised */
-    if (lgw_spi_target == NULL) {
-        DEBUG_MSG("ERROR: CONCENTRATOR UNCONNECTED\n");
         return LGW_REG_ERROR;
     }
 
@@ -1430,7 +1361,7 @@ int lgw_mem_wb(uint16_t mem_addr, const uint8_t *data, uint16_t size) {
         chunk_size = (sz_todo > CHUNK_SIZE_MAX) ? CHUNK_SIZE_MAX : sz_todo;
 
         /* do the burst write */
-        spi_stat += lgw_spi_wb(lgw_spi_target, LGW_SPI_MUX_TARGET_SX1302, addr, &data[chunk_cnt * CHUNK_SIZE_MAX], chunk_size);
+        com_stat = lgw_com_wb(LGW_SPI_MUX_TARGET_SX1302, addr, &data[chunk_cnt * CHUNK_SIZE_MAX], chunk_size);
 
         /* prepare for next write */
         addr += chunk_size;
@@ -1438,8 +1369,8 @@ int lgw_mem_wb(uint16_t mem_addr, const uint8_t *data, uint16_t size) {
         chunk_cnt += 1;
     }
 
-    if (spi_stat != LGW_SPI_SUCCESS) {
-        DEBUG_MSG("ERROR: SPI ERROR DURING REGISTER BURST WRITE\n");
+    if (com_stat != LGW_COM_SUCCESS) {
+        DEBUG_MSG("ERROR: COM ERROR DURING REGISTER BURST WRITE\n");
         return LGW_REG_ERROR;
     } else {
         return LGW_REG_SUCCESS;
@@ -1449,7 +1380,7 @@ int lgw_mem_wb(uint16_t mem_addr, const uint8_t *data, uint16_t size) {
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 int lgw_mem_rb(uint16_t mem_addr, uint8_t *data, uint16_t size, bool fifo_mode) {
-    int spi_stat = LGW_SPI_SUCCESS;
+    int com_stat = LGW_COM_SUCCESS;
     int chunk_cnt = 0;
     uint16_t addr = mem_addr;
     uint16_t sz_todo = size;
@@ -1463,19 +1394,13 @@ int lgw_mem_rb(uint16_t mem_addr, uint8_t *data, uint16_t size, bool fifo_mode) 
         return LGW_REG_ERROR;
     }
 
-    /* check if SPI is initialised */
-    if (lgw_spi_target == NULL) {
-        DEBUG_MSG("ERROR: CONCENTRATOR UNCONNECTED\n");
-        return LGW_REG_ERROR;
-    }
-
     /* read memory by chunks */
     while (sz_todo > 0) {
         /* full or partial chunk ? */
         chunk_size = (sz_todo > CHUNK_SIZE_MAX) ? CHUNK_SIZE_MAX : sz_todo;
 
         /* do the burst read */
-        spi_stat += lgw_spi_rb(lgw_spi_target, LGW_SPI_MUX_TARGET_SX1302, addr, &data[chunk_cnt * CHUNK_SIZE_MAX], chunk_size);
+        com_stat = lgw_com_rb(LGW_SPI_MUX_TARGET_SX1302, addr, &data[chunk_cnt * CHUNK_SIZE_MAX], chunk_size);
 
         /* do not increment the address when the target memory is in FIFO mode (auto-increment) */
         if (fifo_mode == false) {
@@ -1487,8 +1412,8 @@ int lgw_mem_rb(uint16_t mem_addr, uint8_t *data, uint16_t size, bool fifo_mode) 
         chunk_cnt += 1;
     }
 
-    if (spi_stat != LGW_SPI_SUCCESS) {
-        DEBUG_MSG("ERROR: SPI ERROR DURING REGISTER BURST READ\n");
+    if (com_stat != LGW_COM_SUCCESS) {
+        DEBUG_MSG("ERROR: COM ERROR DURING REGISTER BURST READ\n");
         return LGW_REG_ERROR;
     } else {
         return LGW_REG_SUCCESS;
